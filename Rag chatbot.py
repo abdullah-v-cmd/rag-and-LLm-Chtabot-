7b9105c12317874de1263919d6fbebb25c30af3d
Abdullah Naveed <abdullah2w1w@gmail.com>
2026-01-26 09:44:04 +0500

    Add files via upload

---
+++ Rag chatbot.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+loader = PyPDFLoader("D:\\Rag and LLm\\sodapdf-converted.pdf")
+doc=loader.load()
+text=RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=200
+)
+documents=text.split_documents(doc)
+print(f"Number of documents:{len(documents)}")  
+embedding=HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+db=FAISS.from_documents(documents,embedding)
+db.save_local("faiss_index")
+llm=ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    temperature=0
+)
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=db.as_retriever(),
+    return_source_documents=True
+)
+query = "What is Rag chatbot?"
+result = qa.run(query)
+print("Answer:", result)
